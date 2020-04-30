f11a344ba1f0048c9740f98f9eec813517409f0d
Evilwarlock <ceaxyz@gmail.com>
2020-04-30 15:37:22 -0700

    binary search added

--- 852.py
+++ 852.py
@@ -1,13 +1,32 @@
 # 852. Peak Index in a Mountain Array
 
+# class Solution:
+#     def peakIndexInMountainArray(self, A: List[int]) -> int:
+#         # brutal force
+#         for i in range(len(A)):
+#             if A[i] > A[i+1]:
+#                 return A[i]
+        
+
 class Solution:
     def peakIndexInMountainArray(self, A: List[int]) -> int:
-        # brutal force
-        for i in range(len(A)):
-            if A[i] > A[i+1]:
-                return A[i]
+        start = 0
+        mid = 0
+        end = len(A)
+
+        while (start + 1 < end):
+            mid = int(start + (end - start) / 2)
+            if A[mid] > A[mid + 1]:
+                end = mid
+            elif A[mid] < A[mid + 1]:
+                start = mid
         
-        
+        return start + 1
+
+
+
+
+
 # binary search        
         
 #         while (start + 1 < end):
