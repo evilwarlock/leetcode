3fae375efefb2fa90dbdb792ea97426e041c7e0d
Evilwarlock <ceaxyz@gmail.com>
2020-07-22 12:48:37 -0700

    init

--- 79_wordsearch.py
+++ 79_wordsearch.py
@@ -1,18 +1,23 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
+        # check empty board
         if not board:
             return False
+    
         row, col = len(board), len(board[0])
         
-                    
+        # dfs search             
         def dfs(idx, i, j, pos):
             if i < 0 or i >= row or j < 0 or j >= col or board[i][j] != word[idx]:
                 return False
             pos = pos + 1
+            # backtrack
             tmp = board[i][j]
             board[i][j] = '*'
+            # check end condition
             if pos == len(word):
                 return True
+            # search neighbor
             if dfs(idx+1, i+1, j, pos):
                 return True
             if dfs(idx+1, i-1, j, pos):
@@ -21,6 +26,7 @@ class Solution:
                 return True
             if dfs(idx+1, i, j-1, pos):
                 return True
+            # backtrack
             board[i][j] = tmp
             return False
         
