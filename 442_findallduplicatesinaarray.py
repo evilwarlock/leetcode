87cf4d5312c7c9db39fd2c6ecf61710cd4dce5bf
Evilwarlock <ceaxyz@gmail.com>
2020-08-06 14:50:52 -0700

    add optimal solution

--- 442_findallduplicatesinaarray.py
+++ 442_findallduplicatesinaarray.py
@@ -1,16 +1,30 @@
+# class Solution:
+#     def findDuplicates(self, nums: List[int]) -> List[int]:
+#         if not List:
+#             return False
+#         n = len(nums) + 1
+#         count, res = [0] * n, []
+#         for i in nums:
+#             count[i] = count[i] + 1
+#             if count[i] == 2:
+#                 res.append(i)
+                
+#         return res
+            
+        
+# follow up in O(n) with no extra memory?
+# 1. rearrange nums in order
+# 2. hash nums by add "-" for visited
+
 class Solution:
     def findDuplicates(self, nums: List[int]) -> List[int]:
         if not List:
             return False
-        n = len(nums) + 1
-        count, res = [0] * n, []
-        for i in nums:
-            count[i] = count[i] + 1
-            if count[i] == 2:
-                res.append(i)
-                
+        res = []
+        for num in nums:
+            if nums[abs(num)-1] > 0:
+                nums[abs(num)-1] = -1 * nums[abs(num)-1]
+            else:
+                res.append(abs(num)) 
+
         return res
-            
-        
-# follow up in O(n) with no extra memory?
-#         
