a5899d0e06358e61461c2780882d87a7091e062b
Evilwarlock <ceaxyz@gmail.com>
2020-08-11 15:46:59 -0700

    refine code

--- 274_hindex.py
+++ 274_hindex.py
@@ -1,22 +1,34 @@
 class Solution:
-    def hIndex(self, citations):
-        print(citations)
-        if not len(citations):
-            return -1
-        res = 0
-        citations.sort(reverse=True)
-        # print(citations)
-        if citations[-1] > len(citations):
-            return len(citations)
+    # def hIndex(self, citations):
+    #     print(citations)
+    #     if not len(citations):
+    #         return 0
+    #     res = 0
+    #     citations.sort(reverse=True)
+    #     # print(citations)
+    #     if citations[-1] > len(citations):
+    #         return len(citations)
         
-        for i in range(1, len(citations) + 1):
-            if i > citations[i-1]:
-                res = i - 1
-                break
-        return res
+    #     for i in range(1, len(citations) + 1):
+    #         if i > citations[i-1]:
+    #             res = i - 1
+    #             break
+    #     return res
+
+    def hIndex(self, citations):
+        if not len(citations):
+            return 0
+        
+        citations.sort(reverse=True)
+        for idx, cit in enumerate(citations):
+            if cit <= idx:
+                return idx
+        return len(citations)  
+
+
 
 
 test = Solution()
 testcase = [3,0,6,1,5]
-print(testcase.sort())
+# print(testcase.sort())
 print(test.hIndex(testcase))
